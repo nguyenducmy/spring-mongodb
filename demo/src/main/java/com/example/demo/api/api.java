4da82e8956bc43077ee269d3d5335c3e1b414211
myn1008199x@gmail.com <myn1008199x@gmail.com>
2023-05-22 01:07:04 +0700

    create some query

--- demo/src/main/java/com/example/demo/api/api.java
+++ demo/src/main/java/com/example/demo/api/api.java
@@ -4,6 +4,9 @@ import com.example.demo.entity.Car;
 //import com.example.demo.repository.CarRepository;
 import com.example.demo.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +15,8 @@ import java.util.List;
 public class api {
     @Autowired
     CarRepository carRepository;
+    @Autowired
+    MongoTemplate mongoTemplate;
     @PostMapping("/new-car")
     public String newCar(@RequestBody Car car){
         carRepository.save(car);
@@ -20,6 +25,13 @@ public class api {
     @GetMapping("/cars")
     public List<Car> cars(){
         System.out.println(carRepository.findAll());
-        return carRepository.findAll();
+        Query query = new Query();
+        query.addCriteria(Criteria.where("brand").is("Toyota"));
+        List<Car> carList = mongoTemplate.find(query, Car.class);
+        for (Car i : carList) {
+            System.out.println(i.getBrand() + " -  " + i.getModel()
+                    +  " -- " + i.getColor() + "  -  " + i.getId());
+        }
+        return carRepository.findToyota();
     }
 }
--- demo/src/main/java/com/example/demo/repository/CarRepository.java
+++ demo/src/main/java/com/example/demo/repository/CarRepository.java
@@ -2,8 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Car;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CarRepository extends MongoRepository<Car, String> {
+    @Query("{'brand' : 'Toyota'}")
+    List<Car> findToyota();
 }
